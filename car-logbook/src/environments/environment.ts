13b16757fd358aa09f4ac8008dcba2fd7de0b711
Kapka Kaloyanova <146618996+KapkaKaloyanova@users.noreply.github.com>
2026-04-01 03:16:37 +0300

    init: initial Angular project setup
    feat: configure Firebase with Authentication and Firestore

---
+++ car-logbook/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyClFpllwT2HmV3r5uwKp1PWVR2lzyYBOGM",
+        authDomain: "car-logbook-95119.firebaseapp.com",
+        projectId: "car-logbook-95119",
+        storageBucket: "car-logbook-95119.firebasestorage.app",
+        messagingSenderId: "1067575820874",
+        appId: "1:1067575820874:web:63a9b5baa62cd1fd5e9cb3"
+    }
+};
