038c9424a7033351aebe44106e57ffe68005506c
Kapka Kaloyanova <146618996+KapkaKaloyanova@users.noreply.github.com>
2026-04-06 16:16:28 +0300

    feat: Add NotificationComponent with HTML and CSS

---
+++ auto-diary/src/app/layout/notification/notification.component.ts
@@ -0,0 +1,27 @@
+import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
+import { NotificationService } from '../../core/services/notification.service';
+import { Notification } from '../../shared/interfaces/notification';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-notification',
+  imports: [],
+  templateUrl: './notification.component.html',
+  styleUrl: './notification.component.css',
+})
+export class NotificationComponent implements OnInit, OnDestroy {
+  notificationService = inject(NotificationService);
+
+  private subscription!: Subscription;
+
+  currentMessage = signal<Notification | null>(null);
+
+  ngOnInit(): void {
+    this.subscription = this.notificationService.notifications$
+      .subscribe(message => this.currentMessage.set(message))
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe()
+  }
+}
