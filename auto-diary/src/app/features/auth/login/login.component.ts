e5996eaee2e43313b513f3d1f0d06f134dd10832
Kapka Kaloyanova <146618996+KapkaKaloyanova@users.noreply.github.com>
2026-04-07 14:58:27 +0300

    feat: Add LoginComponent logic and html using reactive forms

--- auto-diary/src/app/features/auth/login/login.component.ts
+++ auto-diary/src/app/features/auth/login/login.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthService } from '../../../core/services/auth.service';
+import { Router } from '@angular/router';
+import { UserLogin } from '../../../shared/interfaces/user';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  private authService = inject(AuthService);
+  private router = inject(Router);
 
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(4)])
+  })
+
+  onSubmit() {
+    this.authService.login(this.loginForm.value as UserLogin).subscribe({
+      next: () => {
+        this.router.navigate(['/home'])
+      }
+    })
+  }
 }
