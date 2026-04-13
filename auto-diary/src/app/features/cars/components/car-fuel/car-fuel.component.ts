697e7b37e64139cd8d7724fabcc1486076bf96b7
Kapka Kaloyanova <146618996+KapkaKaloyanova@users.noreply.github.com>
2026-04-14 01:09:22 +0300

    feat(fuel): add fuel records table component (Fuel Tab)
    Implemented table view for fuel history.
    Added styling for fuel entries and empty state handling.

--- auto-diary/src/app/features/cars/components/car-fuel/car-fuel.component.ts
+++ auto-diary/src/app/features/cars/components/car-fuel/car-fuel.component.ts
@@ -1,14 +1,43 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input, OnInit, signal } from '@angular/core';
 import { Car } from '../../../../shared/interfaces/car';
+import { FuelService } from '../../../../core/services/fuel.service';
+import { FuelRecord } from '../../../../shared/interfaces/fuel-record';
+import { RouterLink } from '@angular/router';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-car-fuel',
-  imports: [],
+  imports: [RouterLink, DecimalPipe],
   templateUrl: './car-fuel.component.html',
   styleUrl: './car-fuel.component.css',
 })
-export class CarFuelComponent {
-    car = input.required<Car>();
+export class CarFuelComponent implements OnInit {
+  private fuelService = inject(FuelService);
 
+  car = input.required<Car>();
+  fuelRecords = signal<FuelRecord[]>([]);
 
+  ngOnInit() {
+
+    this.fuelService.getFuelRecordsById(this.car()._id)
+      .subscribe({
+        next: fuelRec => this.fuelRecords.set(fuelRec),
+        error: (err) => {
+          console.error('Грешка при получаване на записите:', err);
+        }
+      })
+  }
+
+  onDelete(fuelId: string) {
+    if (confirm('Сигурни ли сте, че искате да изтриете този запис?')) {
+      this.fuelService.deleteFuelRecord(fuelId).subscribe(
+        {
+          next: () => {
+            this.fuelRecords.update(rec => rec.filter(r => r._id !== fuelId));
+          }
+        }
+      );
+    }
+  }
 }
+
