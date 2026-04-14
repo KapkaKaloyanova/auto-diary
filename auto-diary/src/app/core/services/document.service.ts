358a9df5594d6fc82aea8500870cd0f6a53b48eb
Kapka Kaloyanova <146618996+KapkaKaloyanova@users.noreply.github.com>
2026-04-15 01:35:52 +0300

    feat: Add DocumentService for CRUD operations

---
+++ auto-diary/src/app/core/services/document.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { DocumentRecord } from '../../shared/interfaces/document-record';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DocumentService {
+  private httpClient = inject(HttpClient);
+  private apiUrl = 'http://localhost:3030/data/documents';
+
+
+  createDocumentsRecord(documentData: Partial<DocumentRecord>): Observable<DocumentRecord> {
+    return this.httpClient.post<DocumentRecord>(this.apiUrl, documentData);
+  }
+
+
+  getDocumentsRecordsById(carId: string): Observable<DocumentRecord[]> {
+    return this.httpClient.get<DocumentRecord[]>(`${this.apiUrl}?where=carId%3D%22${carId}%22`);
+  }
+
+  getDocumentsRecordById(docRecId: string): Observable<DocumentRecord> {
+    return this.httpClient.get<DocumentRecord>(`${this.apiUrl}/${docRecId}`);
+  }
+
+  deleteDocumentsRecord(docRecId: string): Observable<void> {
+    return this.httpClient.delete<void>(`${this.apiUrl}/${docRecId}`);
+  }
+
+  editDocumentsRecord(docRecId: string, documentData: Partial<DocumentRecord>): Observable<DocumentRecord> {
+    return this.httpClient.put<DocumentRecord>(`${this.apiUrl}/${docRecId}`, documentData);
+  }
+}
