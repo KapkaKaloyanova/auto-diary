85b8a7958a847ee9575ba2757da3ed66d5bef616
Kapka Kaloyanova <146618996+KapkaKaloyanova@users.noreply.github.com>
2026-04-16 02:28:51 +0300

    feat: Add CarServiceComponent child - template and css as cards

--- auto-diary/src/app/features/cars/components/car-service/car-service.component.ts
+++ auto-diary/src/app/features/cars/components/car-service/car-service.component.ts
@@ -1,14 +1,42 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input, OnInit, signal } from '@angular/core';
 import { Car } from '../../../../shared/interfaces/car';
+import { ServiceRecordService } from '../../../../core/services/service-record.service';
+import { RouterLink } from '@angular/router';
+import { ServiceRecord } from '../../../../shared/interfaces/service-record';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-car-service',
-  imports: [],
+  imports: [RouterLink , DecimalPipe],
   templateUrl: './car-service.component.html',
   styleUrl: './car-service.component.css',
 })
-export class CarServiceComponent {
-    car = input.required<Car>();
+export class CarServiceComponent implements OnInit {
+  private serviceRecordService = inject(ServiceRecordService);
+  car = input.required<Car>();
+  serviceRecords = signal<ServiceRecord[]>([]);
 
+  ngOnInit(): void {
+    this.serviceRecordService.getServiceRecordsById(this.car()._id).subscribe({
+      next: (serviceRecord) => {
+        this.serviceRecords.set(serviceRecord);
+      },
+      error: (err) => {
+        console.error('Грешка при получаване на записите:', err);
+      }
+    });
+  }
 
+  onDelete(serviceRecordId: string) {
+    if (confirm('Сигурни ли сте, че искате да изтриете този запис?')) {
+      this.serviceRecordService.deleteServiceRecord(serviceRecordId).subscribe({
+        next: () => {
+          this.serviceRecords.update(rec => rec.filter(r => r._id !== serviceRecordId));
+        },
+        error: (err) => {
+          console.error('Грешка при изтриване на запис:', err);
+        }
+      });
+    }
+  }
 }
